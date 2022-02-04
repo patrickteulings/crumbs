b8e6ad8ef1ade2bba438d8836301a66771537f94
Patrick Teulings <patrickteulings@gmail.com>
2022-02-04 20:23:12 +0100

    removed sw code from vue config

--- vue.config.js
+++ vue.config.js
@@ -11,13 +11,5 @@ module.exports = {
       bacgroundColor: '#ffffff'
     },
     appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
   }
 }
