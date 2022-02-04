62103856b24733ff0580258f5db84464df303525
Patrick Teulings <patrickteulings@gmail.com>
2022-02-04 20:45:23 +0100

    .

--- src/components/carousel/HomepageCarousel.vue
+++ src/components/carousel/HomepageCarousel.vue
@@ -71,7 +71,7 @@ export default defineComponent({
       state.percentageDragged = (state.isDragging) ? ((touchStart.x - touchMove.x) / state.windowProps.width) : 0
       state.targetValue = (state.startPos.x - state.percentageDragged) * 100
 
-      const diff = (state.targetValue - currentPos) * 0.3
+      const diff = (state.targetValue - currentPos) * 0.2
 
       state.newVal = (currentPos + diff)
 
